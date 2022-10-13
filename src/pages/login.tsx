e6023b86d335c39174dfd9856d00900f18eb23b4
GilahGlobal <99908305+GilahGlobal@users.noreply.github.com>
2022-10-13 21:55:22 +0100

    Update login.tsx

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 
-import { controlInput, loginInputData } from '@/constantsutils';
+import { controlInput, loginInputData } from '@constants/utils';
 import { Box, Image, Text } from '@chakra-ui/react';
 
-import { socialMediaIconsData } from '../constants/utils';
+import { socialMediaIconsData } from '@constants/utils';
 
 const Login = () => {
     const [userName, setUserName] = useState('');
