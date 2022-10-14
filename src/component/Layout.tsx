6cd228a5aaf1cb95eac808a96265d9615d4f26ab
Nwanochie Emmanuel <53648442+wenotch@users.noreply.github.com>
2022-10-14 18:11:03 +0100

    edits

--- src/component/Layout.tsx
+++ src/component/Layout.tsx
@@ -1,17 +1,17 @@
-import Head from 'next/head';
-import React from 'react';
+import Head from "next/head";
+import React from "react";
 
-import { Box } from '@chakra-ui/react';
+import { Box } from "@chakra-ui/react";
 
-const Layout = ({children}: {children: any}) => {
-    return (
-        <Box>
-            <Head>
-                <title>Clique</title>
-            </Head>
-            <Box>{children}</Box>
-        </Box>
-    );
+const Layout = ({ children }: { children: any }) => {
+  return (
+    <Box>
+      <Head>
+        <title>Clique</title>
+      </Head>
+      <Box>{children}</Box>
+    </Box>
+  );
 };
 
 export default Layout;
