39e07875f5fde85df097f5347d4c9f364b9c331a
Mgbemele Ikechukwu <mgbemeleikechukwu@gmail.com>
2022-10-14 17:26:01 +0100

    changed somethings

--- src/constants/utils.ts
+++ src/constants/utils.ts
@@ -1,12 +1,3 @@
-import ChannelIcon from '@icons/ChannelIcon';
-import DiscoverIcon from '@icons/DiscoverIcon';
-import HomeIcon from '@icons/HomeIcon';
-import LiveIcon from '@icons/LiveIcon';
-import ProfileIcon from '@icons/ProfileIcon';
-import SettingsIcon from '@icons/SettingsIcon';
-import TrendingIcon from '@icons/TrendingIcon';
-import WalletIcon from '@icons/WalletIcon';
-
 export const loginInputData: (
     | {
           key: string;
@@ -193,57 +184,5 @@ export const helpGrowData: {
         header: 'Optimized for growth',
         image: 'movie-action',
         bigImage: 'big-movie-action',
-    },
-];
-
-export const menu: {
-    name: string;
-    icon: any;
-}[] = [
-    {
-        name: 'home',
-        icon: HomeIcon,
-    },
-    {
-        name: 'discover',
-        icon: DiscoverIcon,
-    },
-    {
-        name: 'trending',
-        icon: TrendingIcon,
-    },
-    {
-        name: 'profile',
-        icon: ProfileIcon,
-    },
-    {
-        name: 'wallet',
-        icon: WalletIcon,
-    },
-    {
-        name: 'Live Events',
-        icon: LiveIcon,
-    },
-    {
-        name: 'Your channel',
-        icon: ChannelIcon,
-    },
-    {
-        name: 'settings',
-        icon: SettingsIcon,
-    },
-];
-
-export const subcribees: {
-    name: string;
-    imgUrl: string;
-}[] = [
-    {
-        name: 'burnaboy',
-        imgUrl: 'https://bit.ly/prosper-baba',
-    },
-    {
-        name: 'burnaboy',
-        imgUrl: 'https://bit.ly/prosper-baba',
     },
 ];
