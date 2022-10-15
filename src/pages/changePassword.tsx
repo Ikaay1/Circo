36047e97ecb37df025214bce8726565c4da5c8e5
GilahGlobal <99908305+GilahGlobal@users.noreply.github.com>
2022-10-15 10:26:05 +0100

    Update changePassword.tsx

--- src/pages/changePassword.tsx
+++ src/pages/changePassword.tsx
@@ -2,7 +2,7 @@ import { CliqueLogo } from 'component/Navbar';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 
-import { changePasswordInputData, controlInput } from '@/constantsutils';
+import { changePasswordInputData, controlInput } from '@constants/utils';
 import { Box, Image, Text } from '@chakra-ui/react';
 
 import { ShowAuthHeader, ShowAuthImage, SocialMedia } from './login';
