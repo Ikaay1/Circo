5ecec9484b73c238458850aa695b9a4b14c7dc19
Mgbemele Ikechukwu <mgbemeleikechukwu@gmail.com>
2022-10-26 19:53:13 +0100

    implemented updates designers made

--- src/pages/profile/[id]/[name]/index.tsx
+++ src/pages/profile/[id]/[name]/index.tsx
@@ -6,20 +6,20 @@ import Header from '@components/widgets/Header';
 import { profileMenu } from '../../../../constants/utils';
 
 const Profile = () => {
-    const {isOpen, onOpen, onClose} = useDisclosure();
-    return (
-        <Box bg={useColorModeValue('clique.white', 'clique.primaryBg')}>
-            <Header upload={onOpen} />
-            <Box h={{lg: '90vh'}} display='flex'>
-                <Box flex='1' h='100%'>
-                    <SideMenu menu={profileMenu} />
-                </Box>
-                <Box flex='5.5' h='100%'>
-                    <Index />
-                </Box>
-            </Box>
+  const {isOpen, onOpen, onClose} = useDisclosure();
+  return (
+    <Box bg={useColorModeValue('clique.white', 'clique.primaryBg')}>
+      <Header upload={onOpen} />
+      <Box h={{lg: '90vh'}} display='flex'>
+        <Box flex='1.3' h='100%'>
+          <SideMenu menu={profileMenu} />
         </Box>
-    );
+        <Box flex='5.5' h='100%'>
+          <Index />
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Profile;
