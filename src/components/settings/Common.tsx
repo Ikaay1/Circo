46c36c30beee4937f93732d88f8d3167ce32d9c6
osigie <kenosagie88@gmail.com>
2022-11-07 08:19:00 +0100

    added faq

--- src/components/settings/Common.tsx
+++ src/components/settings/Common.tsx
@@ -5,6 +5,7 @@ type Props = {
   subTitle: string;
   main: string;
   highlight: string;
+  firstHighlight?: string;
 };
 
 function Common(props: Props) {
@@ -15,6 +16,7 @@ function Common(props: Props) {
       </Text>
       <Text fontSize={"subHead"} mb="5">
         {props.subTitle}
+        <span style={{ color: "#3088D9" }}> {props.firstHighlight}</span>
       </Text>
       <Text fontSize={"xsl"} mb="10">
         {props.main}
