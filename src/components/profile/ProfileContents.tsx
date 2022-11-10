376abd5467723dda7ad7e7d22af1e6347965a625
Nwanochie Emmanuel <53648442+wenotch@users.noreply.github.com>
2022-11-10 15:56:46 +0100

    fixed mixed import on liveevent

--- src/components/profile/ProfileContents.tsx
+++ src/components/profile/ProfileContents.tsx
@@ -7,22 +7,24 @@ import CliqueTabPanel from '@components/widgets/CliqueTabPanel';
 import { profileNav } from '@constants/utils';
 
 import Playlists from './Playlists';
+import { useGetUserContentsQuery } from 'redux/services/content.service';
 
 const ProfileContents = () => {
   const [route, setRoute] = useState('paid');
+  const { data, isLoading } = useGetUserContentsQuery("");
 
   return (
     <>
-      <Box borderBottom={'1px solid rgba(255, 255, 255, 0.1)'} display='flex'>
-        {profileNav.map(({title, name}) => (
+      <Box borderBottom={"1px solid rgba(255, 255, 255, 0.1)"} display="flex">
+        {profileNav.map(({ title, name }) => (
           <Text
-            mr={'3rem'}
-            lineHeight='24px'
-            color='clique.white'
-            pb={'.8rem'}
-            borderBottom={route === name ? '4px solid #892CDC' : 'none'}
-            cursor={'pointer'}
-            key={'name'}
+            mr={"3rem"}
+            lineHeight="24px"
+            color="clique.white"
+            pb={".8rem"}
+            borderBottom={route === name ? "4px solid #892CDC" : "none"}
+            cursor={"pointer"}
+            key={"name"}
             onClick={() => setRoute(name)}
           >
             {title}
@@ -30,19 +32,19 @@ const ProfileContents = () => {
         ))}
       </Box>
 
-      {route === 'paid' && (
-        <Box mt={'1.5rem'}>
+      {route === "paid" && (
+        <Box mt={"1.5rem"}>
           <Tabs
-            variant={'unstyled'}
-            minW='full'
-            fontFamily='Poppins'
-            color={'clique.white'}
+            variant={"unstyled"}
+            minW="full"
+            fontFamily="Poppins"
+            color={"clique.white"}
           >
             <TabPanels>
               <CliqueTabPanel>
                 <SimpleGrid
-                  columns={{base: 3, lg: 4, mlg: 4, xl: 5}}
-                  spacing='30px'
+                  columns={{ base: 3, lg: 4, mlg: 4, xl: 5 }}
+                  spacing="30px"
                 >
                   <EventModal />
                   <EventModal />
@@ -55,18 +57,15 @@ const ProfileContents = () => {
         </Box>
       )}
 
-      {route === 'play' && (
-        <Box mt={'2.5rem'}>
+      {route === "play" && (
+        <Box mt={"2.5rem"}>
           <Playlists newPlaylist={true} />
         </Box>
       )}
 
-      {route === 'saved' && (
-        <Box mt={'2.3rem'}>
-          <VideoGrid
-            width={'100%'}
-            videos={['videoImage', 'videoImage1', 'videoImage2', 'videoImage3']}
-          />
+      {route === "saved" && (
+        <Box mt={"2.3rem"}>
+          <VideoGrid width={"100%"} videos={data.data.videos} />
         </Box>
       )}
     </>
