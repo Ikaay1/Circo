30dc104d960e40b11d35f8ac08cfd6398976ba87
Ken Osagie <kenosagie88@gmail.com>
2022-11-14 12:53:42 +0100

    added empty state

--- src/components/channel/ChannelContents.tsx
+++ src/components/channel/ChannelContents.tsx
@@ -1,34 +1,34 @@
-import { useState } from 'react';
-import { useGetUserContentsQuery } from 'redux/services/content.service';
+import { useState } from "react";
+import { useGetUserContentsQuery } from "redux/services/content.service";
 
 import { Box, Text } from "@chakra-ui/react";
+import EmptyState from "@components/emptyState/EmptyState";
 import VideoGrid from "@components/home/VideoGrid";
 import Playlists from "@components/profile/Playlists";
 import { channelNav } from "@constants/utils";
 
 const Contents = () => {
-  const [route, setRoute] = useState('upload');
-  const {data, isLoading} = useGetUserContentsQuery('');
-console.log({data})
+  const [route, setRoute] = useState("upload");
+  const { data, isLoading } = useGetUserContentsQuery("");
   return (
     <>
       {isLoading || !data ? (
-        <Box>loading...</Box>
+        <Box> Loading</Box>
       ) : (
         <>
           <Box
-            borderBottom={'1px solid rgba(255, 255, 255, 0.1)'}
-            display='flex'
+            borderBottom={"1px solid rgba(255, 255, 255, 0.1)"}
+            display="flex"
           >
-            {channelNav.map(({title, name}) => (
+            {channelNav.map(({ title, name }) => (
               <Text
-                mr={'3rem'}
-                lineHeight='24px'
-                color='clique.white'
-                pb={'.8rem'}
-                borderBottom={route === name ? '4px solid #892CDC' : 'none'}
-                cursor={'pointer'}
-                key={'name'}
+                mr={"3rem"}
+                lineHeight="24px"
+                color="clique.white"
+                pb={".8rem"}
+                borderBottom={route === name ? "4px solid #892CDC" : "none"}
+                cursor={"pointer"}
+                key={"name"}
                 onClick={() => setRoute(name)}
               >
                 {title}
@@ -36,14 +36,14 @@ console.log({data})
             ))}
           </Box>
 
-          {route === 'playlist' && (
-            <Box mt={'2.5rem'}>
+          {route === "playlist" && (
+            <Box mt={"2.5rem"}>
               <Playlists newPlaylist={false} />
             </Box>
           )}
 
-          {route === 'live' && (
-            <Box mt={'2.3rem'}>
+          {route === "live" && (
+            <Box mt={"2.3rem"}>
               {/* <VideoGrid
             width={'100%'}
             videos={['videoImage', 'videoImage1', 'videoImage2', 'videoImage3']}
@@ -51,10 +51,18 @@ console.log({data})
             </Box>
           )}
 
-          {route === 'upload' && (
-            <Box mt={'2.3rem'}>
-              <VideoGrid width={'100%'} videos={data.data.videos} />
-            </Box>
+          {route === "upload" && (
+            <>
+              {data?.data?.videos?.length === 0 ? (
+                <Box mt="4">
+                  <EmptyState msg="Oops! No post yet. Upload a video!" />
+                </Box>
+              ) : (
+                <Box mt={"2.3rem"}>
+                  <VideoGrid width={"100%"} videos={data?.data?.videos} />
+                </Box>
+              )}
+            </>
           )}
         </>
       )}
--- src/schemas/channel.schema.ts
+++ src/schemas/channel.schema.ts
@@ -1,8 +1,8 @@
 import * as yup from "yup";
 
 export const createChannelSchema = yup.object().shape({
-  name: yup.string().required("Channel name is required"),
-  bioDescription: yup.string().required("Channel bio description is required"),
+  name: yup.string().min(3).max(30).required("Channel name is required"),
+  bioDescription: yup.string().min(5).required("Channel bio description is required"),
   subscriptionFee: yup.number().positive().integer().required("Channel subscription fee is required"),
-  subscriptionInfo: yup.string().required("Channel subscription info is required"),
+  subscriptionInfo: yup.string().min(5).required("Channel subscription info is required"),
 });
