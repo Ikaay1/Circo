11eef14fabde85a20de920bec4ac0a032b001ec6
Nwanochie Emmanuel <53648442+wenotch@users.noreply.github.com>
2022-11-15 18:01:37 +0100

    moved base url to env

--- src/constants/utils.ts
+++ src/constants/utils.ts
@@ -445,8 +445,8 @@ export const playListData = [
     name: "Ayra Star Mix",
   },
 ];
-export const baseUrl: string = "https://clique-backend-dev.onrender.com/";
-// export const baseUrl: string = 'http://localhost:4000/';
+
+export const baseUrl = process.env.NEXT_PUBLIC_BASEURL;
 
 export const scrollBarStyle = {
   "&::-webkit-scrollbar": {
