bed44d5c9ba773ed769974e263b513363c77101d
Nwanochie Emmanuel <53648442+wenotch@users.noreply.github.com>
2022-11-15 10:31:01 +0100

    fixes

--- src/components/home/HoverCard.tsx
+++ src/components/home/HoverCard.tsx
@@ -35,8 +35,7 @@ function HoverCard({
   console.log("video is...", video);
 
   return (
-    <Box
-      boxShadow="0px 0px 10px 0px rgba(0,0,0,0.75)"
+    <Box 
       onMouseLeave={() => setIsHover(false)}
       onClick={() => {
         if (isSubscribed) {
