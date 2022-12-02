bdf9512a2f368e7eeeb62c0fedc8917842cfea47
Nwanochie Emmanuel <53648442+wenotch@users.noreply.github.com>
2022-12-02 14:35:45 +0100

    popular creatos update

--- src/components/trending/CreatorAvatarBox.tsx
+++ src/components/trending/CreatorAvatarBox.tsx
@@ -1,34 +1,43 @@
-import React from 'react';
+import React from "react";
 
-import { Avatar, Box, Flex, Text } from '@chakra-ui/react';
-import AvataWithSpace from '@components/widgets/AvataWithSpace';
+import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
+import AvataWithSpace from "@components/widgets/AvataWithSpace";
+import { useRouter } from "next/router";
 
-function CreatorAvatarBox({user}: {user: any}) {
+function CreatorAvatarBox({ user }: { user: any }) {
+  const router = useRouter();
   return (
-    <Flex justifyContent={'center'} alignItems='center' flexDir={'column'}>
+    <Flex
+      cursor={"pointer"}
+      justifyContent={"center"}
+      onClick={() => router.push(`/channel/subscribe/${user?._id}`)}
+      alignItems="center"
+      flexDir={"column"}
+    >
       {user.photo ? (
         <AvataWithSpace
-          name='Prosper Otemuyiwa'
+          name="Prosper Otemuyiwa"
           url={user.photo}
-          size='45px'
-          borderColor='clique.brown'
-          borderThickness='3px'
+          size="45px"
+          borderColor="clique.brown"
+          borderThickness="3px"
         />
       ) : (
         <Avatar
-          size='md'
-          name={user.firstName + ' ' + user.lastName}
-          borderColor='clique.greenYellow'
+          size="md"
+          name={user.firstName + " " + user.lastName}
+          borderColor="clique.greenYellow"
         />
       )}
       <Text
-        mt='5px'
-        fontFamily={'Poppins'}
-        fontSize='smSubHead'
-        color={'clique.lightGrey'}
+        mt="5px"
+        fontFamily={"Poppins"}
+        fontSize="smSubHead"
+        color={"clique.lightGrey"}
         noOfLines={1}
       >
-        {user.userName}
+        {user.userName.slice(0, 8)}
+        {user.userName.length > 8 ? "..." : ""}
       </Text>
     </Flex>
   );
