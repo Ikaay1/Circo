4cc657caa4952e899db155b150ec2f579cbabf94
Nwanochie Emmanuel <53648442+wenotch@users.noreply.github.com>
2022-12-02 14:08:15 +0100

    handle comment like and unlike loaders

--- src/components/player/EachComment.tsx
+++ src/components/player/EachComment.tsx
@@ -4,22 +4,27 @@ import { BiDislike, BiLike } from "react-icons/bi";
 import { VscReport } from "react-icons/vsc";
 import { useAppSelector } from "redux/app/hooks";
 
-import { Box, Flex, Icon, Text } from "@chakra-ui/react";
+import { Box, Flex, Icon, Spinner, Text } from "@chakra-ui/react";
 import AvataWithSpace from "@components/widgets/AvataWithSpace";
 
 import commentInterface from "../../constants/utils";
 import ReportModal from "./ReportModal";
+import {
+  useDislikeContentCommentMutation,
+  useLikeContentCommentMutation,
+} from "redux/services/content.service";
 
-function EachComment({
-  comment,
-  handleLikeComment,
-  handleDislikeComment,
-}: {
-  comment: commentInterface;
-  handleLikeComment: (id: string) => void;
-  handleDislikeComment: (id: string) => void;
-}) {
+function EachComment({ comment }: { comment: commentInterface }) {
+  const [likeComment, likeInfo] = useLikeContentCommentMutation();
+  const [dislikeComment, dislikeInfo] = useDislikeContentCommentMutation();
   const { userProfile } = useAppSelector((store) => store.app.userReducer);
+  const handleLikeComment = async (id: string) => {
+    await likeComment({ commentId: id });
+  };
+
+  const handleDislikeComment = async (id: string) => {
+    await dislikeComment({ commentId: id });
+  };
   return (
     <Flex mt="15px" bg="clique.ashGrey" rounded="10px" p="20px">
       <AvataWithSpace
@@ -72,18 +77,22 @@ function EachComment({
         <Flex alignItems={"center"} justifyContent="space-between" mt="15px">
           <Flex alignItems={"center"}>
             <Flex cursor={"pointer"} alignItems={"center"}>
-              <Box onClick={() => handleLikeComment(comment._id)}>
-                <Icon
-                  color={
-                    comment.comment.likes.includes(userProfile?._id)
-                      ? "clique.base"
-                      : "clique.white"
-                  }
-                  mr="5px"
-                  fontSize="20px"
-                  as={BiLike}
-                />
-              </Box>
+              {likeInfo.isLoading ? (
+                <Spinner size={"sm"} mr="5px" bg="clique.base" />
+              ) : (
+                <Box onClick={() => handleLikeComment(comment._id)}>
+                  <Icon
+                    color={
+                      comment.comment.likes.includes(userProfile?._id)
+                        ? "clique.base"
+                        : "clique.white"
+                    }
+                    mr="5px"
+                    fontSize="20px"
+                    as={BiLike}
+                  />
+                </Box>
+              )}
               <Text
                 color={"clique.white"}
                 fontFamily={"Poppins"}
@@ -96,18 +105,22 @@ function EachComment({
             </Flex>
 
             <Flex cursor={"pointer"} mx="20px" alignItems={"center"}>
-              <Box onClick={() => handleDislikeComment(comment._id)}>
-                <Icon
-                  color={
-                    comment.comment.dislikes.includes(userProfile?._id)
-                      ? "clique.base"
-                      : "clique.white"
-                  }
-                  mr="5px"
-                  fontSize="smHead"
-                  as={BiDislike}
-                />
-              </Box>
+              {dislikeInfo.isLoading ? (
+                <Spinner size={"sm"} mr="5px" bg="clique.base" />
+              ) : (
+                <Box onClick={() => handleDislikeComment(comment._id)}>
+                  <Icon
+                    color={
+                      comment.comment.dislikes.includes(userProfile?._id)
+                        ? "clique.base"
+                        : "clique.white"
+                    }
+                    mr="5px"
+                    fontSize="smHead"
+                    as={BiDislike}
+                  />
+                </Box>
+              )}
               <Text
                 color={"clique.white"}
                 fontFamily={"Poppins"}
--- src/pages/player/[id].tsx
+++ src/pages/player/[id].tsx
@@ -1,35 +1,33 @@
-import HomeLayout from 'layouts/HomeLayout';
-import { useRouter } from 'next/router';
-import React, { useEffect } from 'react';
-import { useAppSelector } from 'redux/app/hooks';
+import HomeLayout from "layouts/HomeLayout";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import { useAppSelector } from "redux/app/hooks";
 import {
-	useCreateViewMutation,
-	useDislikeContentCommentMutation,
-	useDislikeContentMutation,
-	useGetContentQuery,
-	useLikeContentCommentMutation,
-	useLikeContentMutation,
-} from 'redux/services/content.service';
+  useCreateViewMutation,
+  useDislikeContentCommentMutation,
+  useDislikeContentMutation,
+  useGetContentQuery,
+  useLikeContentCommentMutation,
+  useLikeContentMutation,
+} from "redux/services/content.service";
 
-import { Box, Flex } from '@chakra-ui/react';
-import CommentSection from '@components/player/CommentSection';
-import VideoDetails from '@components/player/VideoDetails';
-import VideoPlayer from '@components/player/VideoPlayer';
+import { Box, Flex } from "@chakra-ui/react";
+import CommentSection from "@components/player/CommentSection";
+import VideoDetails from "@components/player/VideoDetails";
+import VideoPlayer from "@components/player/VideoPlayer";
 
 function Index() {
   const router = useRouter();
-  const {id} = router.query;
-  const {data, isLoading, refetch} = useGetContentQuery(id);
+  const { id } = router.query;
+  const { data, isLoading, refetch } = useGetContentQuery(id);
   const [view] = useCreateViewMutation();
   const [like] = useLikeContentMutation();
   const [dislike] = useDislikeContentMutation();
-  const [likeComment] = useLikeContentCommentMutation();
-  const [dislikeComment] = useDislikeContentCommentMutation();
-  const {userProfile} = useAppSelector((store) => store.app.userReducer);
+  const { userProfile } = useAppSelector((store) => store.app.userReducer);
 
   useEffect(() => {
     const createView = async () => {
-      await view({video_id: data.data.preference.video._id});
+      await view({ video_id: data.data.preference.video._id });
     };
     if (data) {
       if (data.data.preference.video.uploader_id._id !== userProfile?._id) {
@@ -39,22 +37,12 @@ function Index() {
   }, [data, view, userProfile?._id, refetch]);
 
   const handleLike = async () => {
-    await like({video_id: data.data.preference.video._id});
+    await like({ video_id: data.data.preference.video._id });
     refetch();
   };
 
   const handleDislike = async () => {
-    await dislike({video_id: data.data.preference.video._id});
-    refetch();
-  };
-
-  const handleLikeComment = async (id: string) => {
-    await likeComment({commentId: id});
-    refetch();
-  };
-
-  const handleDislikeComment = async (id: string) => {
-    await dislikeComment({commentId: id});
+    await dislike({ video_id: data.data.preference.video._id });
     refetch();
   };
 
@@ -66,25 +54,25 @@ function Index() {
         <HomeLayout>
           <Flex>
             <Box
-              maxH={'90vh'}
-              pb='50px'
-              px='30px'
-              maxW={'calc(100vw - 400px)'}
-              w={'calc(100vw - 400px)'}
-              overflowY={'scroll'}
-              overflowX={'hidden'}
+              maxH={"90vh"}
+              pb="50px"
+              px="30px"
+              maxW={"calc(100vw - 400px)"}
+              w={"calc(100vw - 400px)"}
+              overflowY={"scroll"}
+              overflowX={"hidden"}
               sx={{
-                '&::-webkit-scrollbar': {
-                  width: '8px',
-                  rounded: 'full',
+                "&::-webkit-scrollbar": {
+                  width: "8px",
+                  rounded: "full",
                 },
-                '&::-webkit-scrollbar-track': {
-                  boxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
-                  webkitBoxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
+                "&::-webkit-scrollbar-track": {
+                  boxShadow: "inset 0 0 6px rgba(0,0,0,0.00)",
+                  webkitBoxShadow: "inset 0 0 6px rgba(0,0,0,0.00)",
                 },
-                '&::-webkit-scrollbar-thumb': {
-                  bg: 'clique.primaryBg',
-                  outline: 'none',
+                "&::-webkit-scrollbar-thumb": {
+                  bg: "clique.primaryBg",
+                  outline: "none",
                 },
               }}
             >
@@ -99,11 +87,7 @@ function Index() {
               />
             </Box>
             {/* @ts-ignore */}
-            <CommentSection
-              id={id}
-              handleLikeComment={handleLikeComment}
-              handleDislikeComment={handleDislikeComment}
-            />
+            <CommentSection id={id} />
           </Flex>
         </HomeLayout>
       )}
