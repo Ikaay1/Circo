6703eec12a00234eeffb0a44d8d13346fee0f49e
Nwanochie Emmanuel <53648442+wenotch@users.noreply.github.com>
2022-12-29 14:01:21 +0100

    fixes

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: false,
-  experimental: {
-    esmExternals: false,
-  },
+    reactStrictMode: true,
+    swcMinify: false,
 };
 
 module.exports = nextConfig;
