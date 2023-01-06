e940a8c68d2486fbfcb5caa6de42e581c506e231
Nwanochie Emmanuel <53648442+wenotch@users.noreply.github.com>
2023-01-06 11:55:19 +0100

    light mode in progress

--- src/components/wallet/BeneficiariesCard.tsx
+++ src/components/wallet/BeneficiariesCard.tsx
@@ -1,6 +1,14 @@
-import { Box, Divider, Flex, Icon, Text } from '@chakra-ui/react';
-import Btn from '@components/Button/Btn';
-import tickSquare from '@icons/UploadIcon copy';
+import {
+  Box,
+  Divider,
+  Flex,
+  Icon,
+  Text,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import Btn from "@components/Button/Btn";
+import Color from "@constants/color";
+import tickSquare from "@icons/UploadIcon copy";
 
 type Props = {
   onClick: () => void;
@@ -8,39 +16,41 @@ type Props = {
   walletData: any;
 };
 
-function BeneficiariesCard({onClick, hasBeneficiary, walletData}: Props) {
+function BeneficiariesCard({ onClick, hasBeneficiary, walletData }: Props) {
+  const value = useColorModeValue("clique.lightPrimaryBg", "clique.blackGrey");
   return (
-    <Box pt='7' pb='5' mb='5' bg='clique.black' borderRadius='xl' px='6'>
-      <Text fontSize={'smHead'} mb='2'>
+    <Box
+      pt="7"
+      pb="5"
+      mb="5"
+      bg={Color().whiteAndBlack}
+      borderRadius="xl"
+      px="6"
+    >
+      <Text fontSize={"smHead"} mb="2">
         Beneficiaries
       </Text>
-      <Divider mb='5' />
+      <Divider mb="5" />
       {hasBeneficiary ? (
-        <Flex
-          bg='clique.blackGrey'
-          borderRadius='xl'
-          p='2'
-          align='center'
-          mb='7'
-        >
-          <Icon as={tickSquare} mr='3' fontSize={'l'}></Icon>
+        <Flex bg={value} borderRadius="xl" p="2" align="center" mb="7">
+          <Icon as={tickSquare} mr="3" fontSize={"l"}></Icon>
 
           <Box>
-            <Text fontSize={'smSubHead'}>
+            <Text fontSize={"smSubHead"}>
               {walletData?.beneficiary?.accountName}
             </Text>
-            <Flex color='clique.text'>
-              <Text mr='2' fontSize={'xs'}>
+            <Flex color="clique.text">
+              <Text mr="2" fontSize={"xs"}>
                 {walletData?.beneficiary?.bankName}
               </Text>
-              <Text fontSize={'xs'}>
+              <Text fontSize={"xs"}>
                 {walletData?.beneficiary?.accountNumber}
               </Text>
             </Flex>
           </Box>
         </Flex>
       ) : (
-        <Text fontSize={'xsl'} color='clique.text' align='center' py='5'>
+        <Text fontSize={"xsl"} color="clique.text" align="center" py="5">
           You do not have a beneficiary account to receive your monthly
           automated withdrawal. Kindly add your account number to create a
           beneficiary.
@@ -48,9 +58,9 @@ function BeneficiariesCard({onClick, hasBeneficiary, walletData}: Props) {
       )}
 
       <Btn
-        text={hasBeneficiary ? 'Change beneficiary' : 'Add beneficiary'}
-        style={{width: '100%'}}
-        py='7'
+        text={hasBeneficiary ? "Change beneficiary" : "Add beneficiary"}
+        style={{ width: "100%" }}
+        py="7"
         onClick={onClick}
       ></Btn>
     </Box>
