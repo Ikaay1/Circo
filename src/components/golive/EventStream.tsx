19f1e3ec526183b60b90b58ea4d4f3a26211ccff
Nwanochie Emmanuel <53648442+wenotch@users.noreply.github.com>
2023-01-24 15:09:14 +0100

    Edits

--- src/components/golive/EventStream.tsx
+++ src/components/golive/EventStream.tsx
@@ -35,7 +35,7 @@ function EventStream({ event, setTabIndex }: { event: any; setTabIndex: any }) {
         description: event?.eventId?.description,
         thumbNail: event?.eventId?.thumbNails[0] || "",
         category: event?.eventId?.categoryId,
-        fee: event?.eventId?.paidToWatch ? event?.eventId?.fee : 0,
+        fee: event?.eventId?.fee ?? 0,
         ageRange: event?.eventId?.ageRange,
         schedule: event?.eventId?.schedule,
       }}
@@ -66,6 +66,9 @@ function EventStream({ event, setTabIndex }: { event: any; setTabIndex: any }) {
         formData.append("paidToWatch", values.fee > 0 ? "true" : "false");
         formData.append("category", "SCHEDULE");
         formData.append("categoryId", values.category);
+        formData.append("fee", values.fee);
+        formData.append("schedule", values.schedule);
+
 
         const res: any = await updateEvent({
           id: event?.eventId?._id,
