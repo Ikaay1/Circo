b21bf71db8104b9b8b63f73737ef3ae636ff3c68
Mgbemele Ikechukwu <mgbemeleikechukwu@gmail.com>
2023-03-16 13:15:57 +0100

    updates

--- src/constants/color.tsx
+++ src/constants/color.tsx
@@ -1,39 +1,34 @@
-import React from "react";
+import React from 'react';
 
-import { useColorModeValue } from "@chakra-ui/react";
+import {useColorModeValue} from '@chakra-ui/react';
 
 function Color() {
-  const blackAndWhite = useColorModeValue("clique.black", "clique.whiteGrey");
-  const blackAndPureWhite = useColorModeValue("clique.black", "clique.white");
-  const blackAndWhite2 = useColorModeValue("clique.black", "clique.white");
+  const blackAndWhite = useColorModeValue('clique.black', 'clique.whiteGrey');
+  const blackAndPureWhite = useColorModeValue('clique.black', 'clique.white');
+  const blackAndWhite2 = useColorModeValue('clique.black', 'clique.white');
   const greyAndWhite = useColorModeValue(
-    "clique.lightPrimaryBg",
-    "clique.secondaryGrey1"
+    'clique.lightPrimaryBg',
+    'clique.secondaryGrey1',
   );
-<<<<<<< HEAD
   const greyAndWhite2 = useColorModeValue(
     'clique.white',
     'clique.secondaryGrey1',
   );
   const whiteAndBlack = useColorModeValue('clique.white', 'clique.black');
   const blackAndGrey = useColorModeValue('clique.black', 'clique.grey');
-=======
-  const whiteAndBlack = useColorModeValue("clique.white", "clique.black");
-  const blackAndGrey = useColorModeValue("clique.black", "clique.grey");
->>>>>>> f5e4aa62de7e416e2cf8c2cc4dbd861eccc16626
   const whiteGreyAndBlack = useColorModeValue(
-    "clique.black",
-    "clique.whiteGrey"
+    'clique.black',
+    'clique.whiteGrey',
   );
   const blackAndWhiteGrey = useColorModeValue(
-    "clique.lightPrimaryBg",
-    "clique.grey"
+    'clique.lightPrimaryBg',
+    'clique.grey',
   );
-  const greyAndPureWhite = useColorModeValue("clique.white", "clique.grey");
+  const greyAndPureWhite = useColorModeValue('clique.white', 'clique.grey');
 
   const lightAndPrimary = useColorModeValue(
-    "clique.lightPrimaryBg",
-    "clique.primaryBg"
+    'clique.lightPrimaryBg',
+    'clique.primaryBg',
   );
 
   return {
