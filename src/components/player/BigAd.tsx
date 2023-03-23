6e6a1db2ef404ab41f4898b0c61771a30dae8925
Mgbemele Ikechukwu <mgbemeleikechukwu@gmail.com>
2023-03-23 15:04:17 +0100

    video ad update, video cards update, notification fix

--- src/components/player/BigAd.tsx
+++ src/components/player/BigAd.tsx
@@ -7,17 +7,33 @@ import CloseIcon from '@icons/CloseIcon';
 function BigAd({
   setIsAd,
   setIsSmallAd,
+  isPlay,
+  setIsPlay,
   playerRef,
 }: {
   setIsAd: React.Dispatch<React.SetStateAction<boolean>>;
   setIsSmallAd: React.Dispatch<React.SetStateAction<boolean>>;
   playerRef: any;
+  setIsPlay: React.Dispatch<React.SetStateAction<boolean>>;
+  isPlay: boolean;
 }) {
   return (
     <Box bg='red' minH='calc(100% - 80px)' pos='absolute' zIndex='2' w='full'>
       {/* <Adsense client='ca-pub-2071647719246163' slot='7595819867' /> */}
       <Icon
-        onClick={() => setIsAd(false)}
+        // onClick={() => setIsAd(false)}
+        onClick={
+          playerRef.current
+            ? () => {
+                setIsAd(false);
+                playerRef.current.play();
+                setIsPlay(true);
+              }
+            : () => {
+                setIsPlay(!isPlay);
+                setIsAd(false);
+              }
+        }
         as={CloseIcon}
         w='40px'
         h='40px'
