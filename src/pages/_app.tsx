ffe98633cb30827ba1f7cfe4c10cdb902c41fe9c
Mgbemele Ikechukwu <mgbemeleikechukwu@gmail.com>
2023-03-29 10:53:32 +0100

    google analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,47 +1,48 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import "../styles/globals.css";
+import '../styles/globals.css';
 
-import devtools from "devtools-detect";
+import devtools from 'devtools-detect';
 // import useConsoleOpen from 'hooks/useConsoleOpen';
-import Layout from "layouts/Layout";
-import Router, { useRouter } from "next/router";
-import { useEffect, useState } from "react";
-import { Toaster } from "react-hot-toast";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
-import { io } from "socket.io-client";
+import Layout from 'layouts/Layout';
+import Router, { useRouter } from 'next/router';
+import Script from 'next/script';
+import { useEffect, useState } from 'react';
+import { Toaster } from 'react-hot-toast';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { io } from 'socket.io-client';
 
 import {
-  Button,
-  ChakraProvider,
-  ColorModeScript,
-  useToast,
-} from "@chakra-ui/react";
-import theme from "@constants/theme";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+	Button,
+	ChakraProvider,
+	ColorModeScript,
+	useToast,
+} from '@chakra-ui/react';
+import theme from '@constants/theme';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
-import { Chakra } from "../components/widgets/Chakara";
-import { persistor, store } from "../redux/app/store";
+import { Chakra } from '../components/widgets/Chakara';
+import { persistor, store } from '../redux/app/store';
 
-import type { AppProps } from "next/app";
-const NProgress = require("nprogress");
+import type {AppProps} from 'next/app';
+const NProgress = require('nprogress');
 
-function MyApp({ Component, pageProps }: any) {
+function MyApp({Component, pageProps}: any) {
   const [showChild, setShowChild] = useState(false);
   const toast = useToast();
   const router = useRouter();
   // const consoleOpen = useConsoleOpen();
 
   useEffect(() => {
-    NProgress.configure({ showSpinner: false });
-    Router.events.on("routeChangeStart", (url: any) => {
+    NProgress.configure({showSpinner: false});
+    Router.events.on('routeChangeStart', (url: any) => {
       NProgress.start();
     });
-    Router.events.on("routeChangeComplete", (url: any) => {
+    Router.events.on('routeChangeComplete', (url: any) => {
       NProgress.done();
     });
 
-    Router.events.on("routeChangeError", (url: any) => {
+    Router.events.on('routeChangeError', (url: any) => {
       NProgress.done();
     });
   }, [Router]);
@@ -51,14 +52,14 @@ function MyApp({ Component, pageProps }: any) {
   }, []);
 
   useEffect(() => {
-    console.log("Is DevTools open:", devtools.isOpen);
+    console.log('Is DevTools open:', devtools.isOpen);
   }, [devtools.isOpen]);
 
   if (!showChild) {
     return null;
   }
 
-  if (typeof window === "undefined") {
+  if (typeof window === 'undefined') {
     return <></>;
   }
 
@@ -88,8 +89,27 @@ function MyApp({ Component, pageProps }: any) {
         <Chakra cookies={pageProps.cookies} theme={theme}>
           <GoogleOAuthProvider
             // clientId={process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!}
-            clientId="75252957257-5i6nhuvt2643qo64oo76vg3a9ba4sp5g.apps.googleusercontent.com"
+            clientId='75252957257-5i6nhuvt2643qo64oo76vg3a9ba4sp5g.apps.googleusercontent.com'
           >
+            <Script
+              strategy='afterInteractive'
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+            />
+                
+            <Script
+              id='google-analytics'
+              strategy='afterInteractive'
+              dangerouslySetInnerHTML={{
+                __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}, {
+            page_path: window.location.pathname,
+          });
+        `,
+              }}
+            />
             <Layout>
               <ColorModeScript
                 initialColorMode={theme.config.initialColorMode}
