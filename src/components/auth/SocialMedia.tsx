5945eedebaf9efa5338147edbf6e1ed13cee5dae
Mgbemele Ikechukwu <mgbemeleikechukwu@gmail.com>
2023-04-18 08:25:00 +0100

    updates

--- src/components/auth/SocialMedia.tsx
+++ src/components/auth/SocialMedia.tsx
@@ -1,19 +1,19 @@
-import axios from "axios";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import React from "react";
-import FacebookLogin from "react-facebook-login/dist/facebook-login-render-props";
-import { toast } from "react-hot-toast";
-import { useAppDispatch } from "redux/app/hooks";
+import axios from 'axios';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import React from 'react';
+import FacebookLogin from 'react-facebook-login/dist/facebook-login-render-props';
+import { toast } from 'react-hot-toast';
+import { useAppDispatch } from 'redux/app/hooks';
 import {
-  useLoginMutation,
-  useSocialPreSignupMutation,
-} from "redux/services/auth.service";
-import { setCredentials } from "redux/slices/authSlice";
+	useLoginMutation,
+	useSocialPreSignupMutation,
+} from 'redux/services/auth.service';
+import { setCredentials } from 'redux/slices/authSlice';
 
-import { Box, Button, Image, Text } from "@chakra-ui/react";
-import Color from "@constants/color";
-import { useGoogleLogin } from "@react-oauth/google";
+import { Box, Button, Image, Text } from '@chakra-ui/react';
+import Color from '@constants/color';
+import { useGoogleLogin } from '@react-oauth/google';
 
 export const SocialMedia = ({
   haveAccount,
@@ -26,6 +26,7 @@ export const SocialMedia = ({
   const [login, loginStatus] = useLoginMutation();
   const [socialPreSignup, socialPreSignupStatus] = useSocialPreSignupMutation();
   const dispatch = useAppDispatch();
+  const {next}: any = router.query
 
   const loginGoogle = useGoogleLogin({
     onSuccess: async (tokenResponse) => {
@@ -69,7 +70,7 @@ export const SocialMedia = ({
                 payload: res.data,
               })
             );
-            router.push("/home");
+            next ? router.push(next) : router.push('/home');
           } else {
             toast.error(res.error?.data?.message);
           }
@@ -114,7 +115,7 @@ export const SocialMedia = ({
               payload: res.data,
             })
           );
-          router.push("/home");
+          next ? router.push(next) : router.push('/home');
         } else {
           toast.error(res.error?.data?.message);
           router.push("/login");
