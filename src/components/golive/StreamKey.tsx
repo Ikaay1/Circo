eb3291522b9d1355ce09202cda3e3ce93624f8f9
Mgbemele Ikechukwu <mgbemeleikechukwu@gmail.com>
2023-06-10 17:14:44 +0100

    updates

--- src/components/golive/StreamKey.tsx
+++ src/components/golive/StreamKey.tsx
@@ -1,3 +1,7 @@
+import React, {useEffect} from 'react';
+import {AiOutlineEye, AiOutlineEyeInvisible} from 'react-icons/ai';
+import {useAppSelector} from 'redux/app/hooks';
+
 import {
   Box,
   Button,
@@ -13,40 +17,38 @@ import {
   Select,
   Text,
   useColorModeValue,
-} from "@chakra-ui/react";
-import React, { useEffect } from "react";
-import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
-import { useAppSelector } from "redux/app/hooks";
-import CopyButton from "./CopyButton";
+} from '@chakra-ui/react';
 
-function StreamKey({ streamDetails }: any) {
+import CopyButton from './CopyButton';
+
+function StreamKey({streamDetails}: any) {
   const [showPassword, setShowPassword] = React.useState(false);
-  const value = useColorModeValue("clique.white", "clique.secondaryGrey1");
+  const value = useColorModeValue('clique.white', 'clique.secondaryGrey1');
   return (
-    <Flex flexDir={{ base: "column", lg: "row" }} pb="150px">
-      <Box w={{ base: "full", lg: "50%" }}>
-        <Text mt="20px" fontSize="smSubHead" color="clique.text">
+    <Flex flexDir={{base: 'column', lg: 'row'}} pb='150px'>
+      <Box w={{base: 'full', lg: '50%'}}>
+        <Text mt='20px' fontSize='smSubHead' color='clique.text'>
           Stream key
         </Text>
         <Grid
-          templateColumns="repeat(15, 1fr)"
-          mt="10px"
-          justifyContent={"space-between"}
+          templateColumns='repeat(15, 1fr)'
+          mt='10px'
+          justifyContent={'space-between'}
         >
-          <GridItem colSpan={12} mr="16px">
-            <Box bg={value} px="2" py="3" borderRadius={"10px"}>
-              <InputGroup border={"none"}>
+          <GridItem colSpan={12} mr='16px'>
+            <Box bg={value} px='2' py='3' borderRadius={'10px'}>
+              <InputGroup border={'none'}>
                 <Input
-                  border={"none"}
+                  border={'none'}
                   isReadOnly
-                  type={showPassword ? "text" : "password"}
+                  type={showPassword ? 'text' : 'password'}
                   value={streamDetails?.streamKey}
                 />
                 <InputRightElement>
                   <Icon
-                    cursor={"pointer"}
+                    cursor={'pointer'}
                     onClick={() => setShowPassword(!showPassword)}
-                    fontSize={"20px"}
+                    fontSize={'20px'}
                     as={showPassword ? AiOutlineEye : AiOutlineEyeInvisible}
                   />
                 </InputRightElement>
@@ -56,21 +58,21 @@ function StreamKey({ streamDetails }: any) {
 
           <CopyButton value={streamDetails?.streamKey} />
         </Grid>
-        <Text mt="20px" fontSize="smSubHead" color="clique.text">
+        <Text mt='20px' fontSize='smSubHead' color='clique.text'>
           Streaming ID
         </Text>
         <Grid
-          templateColumns="repeat(15, 1fr)"
-          mt="10px"
-          justifyContent={"space-between"}
+          templateColumns='repeat(15, 1fr)'
+          mt='10px'
+          justifyContent={'space-between'}
         >
-          <GridItem colSpan={12} mr="16px">
-            <Box bg={value} px="2" py="3" borderRadius={"10px"}>
-              <InputGroup border={"none"}>
+          <GridItem colSpan={12} mr='16px'>
+            <Box bg={value} px='2' py='3' borderRadius={'10px'}>
+              <InputGroup border={'none'}>
                 <Input
-                  border={"none"}
+                  border={'none'}
                   isReadOnly
-                  type={"text"}
+                  type={'text'}
                   value={streamDetails?.streamerId}
                 />
               </InputGroup>
@@ -79,21 +81,21 @@ function StreamKey({ streamDetails }: any) {
 
           <CopyButton value={streamDetails?.streamerId} />
         </Grid>
-        <Text mt="20px" fontSize="smSubHead" color="clique.text">
+        <Text mt='20px' fontSize='smSubHead' color='clique.text'>
           Playback Id
         </Text>
         <Grid
-          templateColumns="repeat(15, 1fr)"
-          mt="10px"
-          justifyContent={"space-between"}
+          templateColumns='repeat(15, 1fr)'
+          mt='10px'
+          justifyContent={'space-between'}
         >
-          <GridItem colSpan={12} mr="16px">
-            <Box bg={value} px="2" py="3" borderRadius={"10px"}>
-              <InputGroup border={"none"}>
+          <GridItem colSpan={12} mr='16px'>
+            <Box bg={value} px='2' py='3' borderRadius={'10px'}>
+              <InputGroup border={'none'}>
                 <Input
-                  border={"none"}
+                  border={'none'}
                   isReadOnly
-                  type={"text"}
+                  type={'text'}
                   value={streamDetails?.playbackId}
                 />
               </InputGroup>
@@ -103,32 +105,32 @@ function StreamKey({ streamDetails }: any) {
           <CopyButton value={streamDetails?.playbackId} />
         </Grid>
 
-        <Text mt="20px" fontSize="smSubHead" color="clique.text">
+        <Text mt='20px' fontSize='smSubHead' color='clique.text'>
           RMTP URL
         </Text>
         <Grid
-          templateColumns="repeat(15, 1fr)"
-          mt="10px"
-          justifyContent={"space-between"}
+          templateColumns='repeat(15, 1fr)'
+          mt='10px'
+          justifyContent={'space-between'}
         >
-          <GridItem colSpan={12} mr="16px">
-            <Box bg={value} px="2" py="3" borderRadius={"10px"}>
-              <InputGroup border={"none"}>
+          <GridItem colSpan={12} mr='16px'>
+            <Box bg={value} px='2' py='3' borderRadius={'10px'}>
+              <InputGroup border={'none'}>
                 <Input
-                  border={"none"}
+                  border={'none'}
                   isReadOnly
-                  type={"text"}
-                  value={"rtmps://global-live.mux.com:443/app/ "}
+                  type={'text'}
+                  value={'rtmps://global-live.mux.com:443/app/ '}
                 />
               </InputGroup>
             </Box>
           </GridItem>
 
-          <CopyButton value={"tmps://global-live.mux.com:443/app/ "} />
+          <CopyButton value={'rtmps://global-live.mux.com:443/app/ '} />
         </Grid>
       </Box>
 
-      <Box px="50px"></Box>
+      <Box px='50px'></Box>
     </Flex>
   );
 }
