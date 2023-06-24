42ad5a26f6da1cf02ba8f6bcb1921ef2409d62ff
Nwanochie Emmanuel <53648442+wenotch@users.noreply.github.com>
2023-06-24 12:49:37 +0100

    updates

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import {Box, Icon, Input, Select, Text, useColorMode} from '@chakra-ui/react';
-import Color from '@constants/color';
-import Padlock from '@icons/Padlock';
-import PasswordIcon from '@icons/PasswordIcon';
+import {
+  Box,
+  Flex,
+  Icon,
+  Input,
+  Select,
+  Text,
+  useColorMode,
+} from "@chakra-ui/react";
+import Color from "@constants/color";
+import Padlock from "@icons/Padlock";
+import PasswordIcon from "@icons/PasswordIcon";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 
 const AuthInput = ({
   image,
@@ -22,35 +31,35 @@ const AuthInput = ({
   setAgeRange,
   onBlur,
 }: any) => {
-  const {colorMode, toggleColorMode} = useColorMode();
+  const { colorMode, toggleColorMode } = useColorMode();
   const [isFocused, setIsFocused] = React.useState(false);
   return (
     <>
       {!option ? (
         <Input
           value={theState}
-          h='62px'
-          borderRadius='12px'
+          h="62px"
+          borderRadius="12px"
           onChange={(e) => setTheState(e.target.value)}
-          _focus={{boxShadow: 'none', border: 'none', outline: 'none'}}
-          _active={{boxShadow: 'none', border: 'none', outline: 'none'}}
+          _focus={{ boxShadow: "none", border: "none", outline: "none" }}
+          _active={{ boxShadow: "none", border: "none", outline: "none" }}
           // className='input'
           type={
             !i
               ? image
                 ? showPassword
-                  ? 'text'
-                  : 'password'
+                  ? "text"
+                  : "password"
                 : email
-                ? 'email'
-                : 'text'
+                ? "email"
+                : "text"
               : i === 1
               ? showPassword0
-                ? 'text'
-                : 'password'
+                ? "text"
+                : "password"
               : showPassword1
-              ? 'text'
-              : 'password'
+              ? "text"
+              : "password"
           }
           required={referral ? false : true}
           placeholder={name}
@@ -58,15 +67,15 @@ const AuthInput = ({
           backgroundColor={Color().greyAndPureWhite}
           _placeholder={{
             color: Color().blackAndWhite,
-            fontSize: isFocused || theState !== '' ? 'sm3' : '1rem',
-            pb: isFocused || theState !== '' ? '5px' : '0',
-            transition: 'all .3s ease',
+            fontSize: isFocused || theState !== "" ? "sm3" : "1rem",
+            pb: isFocused || theState !== "" ? "5px" : "0",
+            transition: "all .3s ease",
             transform:
-              isFocused || theState !== ''
-                ? 'translateY(-110%);  '
-                : 'translateY(0%); ',
+              isFocused || theState !== ""
+                ? "translateY(-110%);  "
+                : "translateY(0%); ",
           }}
-          borderWidth={'1px'}
+          borderWidth={"1px"}
           borderColor={Color().greyAndPureWhite}
           onFocus={() => setIsFocused(true)}
           onBlur={
@@ -82,17 +91,17 @@ const AuthInput = ({
         <Select
           value={ageRange}
           bg={Color().greyAndPureWhite}
-          borderWidth={'1px'}
+          borderWidth={"1px"}
           borderColor={Color().greyAndPureWhite}
-          borderRadius='12px'
+          borderRadius="12px"
           //   pl="1.3rem"
-          _focus={{boxShadow: 'none'}}
-          h='60px'
-          w='100%'
+          _focus={{ boxShadow: "none" }}
+          h="60px"
+          w="100%"
           onChange={(e) => setAgeRange(e.target.value)}
-          className='input'
+          className="input"
           required={true}
-          placeholder={'Select ' + name}
+          placeholder={"Select " + name}
           _placeholder={{
             color: Color().blackAndWhite,
           }}
@@ -104,42 +113,49 @@ const AuthInput = ({
           ))}
         </Select>
       )}
-      {theState !== '' && !option && (
+      {theState !== "" && !option && (
         <Text
-          position='absolute'
-          left={'4%'}
-          pt='5px'
-          fontSize='sm3'
+          position="absolute"
+          left={"4%"}
+          pt="5px"
+          fontSize="sm3"
           color={Color().blackAndWhite}
-          transition='all .3s ease'
-          top='0%'
+          transition="all .3s ease"
+          top="0%"
           transform={
-            isFocused || theState !== ''
-              ? 'translateY(0%)'
-              : 'translateY(50%); font-size: 1rem'
+            isFocused || theState !== ""
+              ? "translateY(0%)"
+              : "translateY(50%); font-size: 1rem"
           }
-          zIndex='99'
+          zIndex="99"
         >
           {name}
         </Text>
       )}
 
       {image && (
-        <Box
-          position='absolute'
-          right={'4.5%'}
-          top='50%'
-          transform='translateY(-50%)'
-          cursor={'pointer'}
+        <Flex
+          alignItems={"center"}
+          position="absolute"
+          right={"4.5%"}
+          top="50%"
+          transform="translateY(-50%)"
+          cursor={"pointer"}
           onClick={handleShowPassword}
-          zIndex='99'
+          zIndex="99"
         >
-          {colorMode === 'dark' ? (
-            <Icon as={Padlock} />
+          {colorMode === "dark" ? (
+            showPassword ? (
+              <Icon as={AiFillEyeInvisible} fontSize={"20px"} />
+            ) : (
+              <Icon as={AiFillEye} fontSize={"20px"} />
+            )
+          ) : showPassword ? (
+            <Icon as={AiFillEyeInvisible} fontSize={"20px"} />
           ) : (
-            <Icon as={PasswordIcon} />
+            <Icon as={AiFillEye} fontSize={"20px"} />
           )}
-        </Box>
+        </Flex>
       )}
     </>
   );
